c30535c2b6699d32a14a33894c950da677beffbb
bburritobowl <72266793+bburritobowl@users.noreply.github.com>
2022-01-31 15:37:19 -0600

    Mock Level
    I set up a mock level to run tests, and made two test objects. There are projectile spawners now, and the objects have slightly random velocities.
    NO ERRORS

---
+++ Hillbilly Skeet Shoot/Assets/Scripts/CannonBallLauncher.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBallLauncher : MonoBehaviour
+{
+    public float launchTimer = 5f;
+    public GameObject cannonBall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("LaunchCannonBalls", launchTimer, launchTimer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LaunchCannonBalls()
+    {
+        Instantiate(cannonBall, transform.position, transform.rotation);
+    }
+}
