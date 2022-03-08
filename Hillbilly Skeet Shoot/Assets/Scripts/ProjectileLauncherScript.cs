5136dbfa72dccfd18bad115d758049c8b6860a61
bburritobowl <72266793+bburritobowl@users.noreply.github.com>
2022-03-08 09:15:45 -0600

    Not really working yet
    No errors tho

--- Hillbilly Skeet Shoot/Assets/Scripts/ProjectileLauncherScript.cs
+++ Hillbilly Skeet Shoot/Assets/Scripts/ProjectileLauncherScript.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class ProjectileLauncherScript : MonoBehaviour
 {
     public float launchTimer = 5f;
-    public GameObject[] projectiles;
+    public List<GameObject> projectiles;
+    public GameObject[] secondaryObjects;
+    public GameObject[] explodingObjects;
+    [SerializeField] float timer = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +18,28 @@ public class ProjectileLauncherScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+    }
+
+    void FixedUpdate()
+    {
+        if(Mathf.Approximately(timer, 5f))
+        {
+            projectiles.Add(secondaryObjects[0]);
+        }
+        else if(Mathf.Approximately(timer, 10f))
+        {
+            projectiles.Add(secondaryObjects[1]);
+        }
+        else if(Mathf.Approximately(timer, 15f))
+        {
+            projectiles.Add(explodingObjects[0]);
+        }
     }
 
     void LaunchProjectiles()
     {
-        int randomObject = Random.Range(0, projectiles.Length);
+        int randomObject = Random.Range(0, projectiles.Count);
         Instantiate(projectiles[randomObject], transform.position, transform.rotation);
     }
 }
