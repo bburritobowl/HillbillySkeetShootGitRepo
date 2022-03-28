d727ac8e3f3570180ca2d0d12f81f19662bb5ebd
bburritobowl <72266793+bburritobowl@users.noreply.github.com>
2022-03-28 14:04:04 -0500

    Not sure

---
+++ Hillbilly Skeet Shoot/Assets/Scripts/mainMenuScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenuScript : MonoBehaviour
+{
+    public GameObject SettingsPanel;
+    public GameObject MainMenuPanel;
+    public GameObject CuteOpeningScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CuteOpeningScene.SetActive(true);
+        SettingsPanel.SetActive(false);
+        MainMenuPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        //to be changed later
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void settings()
+    {
+        CuteOpeningScene.SetActive(true);
+        SettingsPanel.SetActive(false);
+        MainMenuPanel.SetActive(false);
+    }
+
+    public void OpeningArt()
+    {
+        //click anywhere to go to main menu panel
+        CuteOpeningScene.SetActive(false);
+        MainMenuPanel.SetActive(true);
+    }
+
+}
