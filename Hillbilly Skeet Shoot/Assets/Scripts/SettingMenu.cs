077ce274d316a45b634b6b1e2cf13e77810fb473
BladeofCadence <BladeofCadence@gmail.com>
2022-04-08 15:21:34 -0500

    audio

--- Hillbilly Skeet Shoot/Assets/Scripts/SettingMenu.cs
+++ Hillbilly Skeet Shoot/Assets/Scripts/SettingMenu.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SettingMenu : MonoBehaviour
 {
-    public void setvolume(float volume)
+    public AudioMixer audioMixerEffects;
+    public AudioMixer audioMixerMusic;
+    public void setvolumeEffects(float volume)
     {
-        Debug.Log(volume);
+        audioMixerEffects.SetFloat("volumeeffects", volume);
     }
 
-
+    public void setvolumeMusic(float volume)
+    {
+        audioMixerMusic.SetFloat("volumemusic", volume);
+    }
 }
